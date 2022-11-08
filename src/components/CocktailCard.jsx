a337e66566f63d4e3ccb8174a061a2119468a8b7
Victor Martinez <victorlorenzomartinez@gmail.com>
2022-11-08 16:04:23 +0100

    added delete button and function to cocktailcard

--- src/components/CocktailCard.jsx
+++ src/components/CocktailCard.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import supabase from '../config/supabaseClient'
 
 const CocktailCard = ({ cocktail }) => {
+    
+    const handleDelete = async() => {
+        const {data, error} = await supabase
+            .from('cocktails')
+            .delete()
+            .eq('id', cocktail.id)
+            .select()
+
+        if(error){
+            console.log(error)
+        }
+        if(data){
+            console.log(data)
+        }
+    }
+
     return (
         <div className='cocktail-card'>
             <h3>{cocktail.name}</h3>
@@ -11,6 +28,7 @@ const CocktailCard = ({ cocktail }) => {
                 <Link to={`/${cocktail.id}`}>
                     <i className='material-icons'>edit</i>
                 </Link>
+                <i className='material-icons' onClick={handleDelete}>delete</i>
             </div>
         </div>
     )
